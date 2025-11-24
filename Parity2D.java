45c3c830a49996e9688909c77229e96c831aece4
Gnanika Omkarini Makkena <gnanikaomkarini@gmail.com>
2025-11-24 20:53:01 +0530

    src: Hamming

--- Parity2D.java
+++ Parity2D.java
@@ -2,36 +2,41 @@ import java.util.*;
 
 public class Parity2D {
     public static void main(String[] a){
-        Scanner s=new Scanner(System.in);
-        int r,c;
-        r=s.nextInt(); c=s.nextInt(); // matrix size
-        int[][] m=new int[r][c];
+        Scanner s = new Scanner(System.in);
 
-        // input
-        for(int i=0;i<r;i++)
-            for(int j=0;j<c;j++)
-                m[i][j]=s.nextInt();
+        int r = s.nextInt(), c = s.nextInt();
+        int[][] send = new int[r][c];
+        int[][] recv = new int[r][c];
 
-        // sender: add row parity & column parity
-        int[] rp=new int[r], cp=new int[c];
-        for(int i=0;i<r;i++)
-            for(int j=0;j<c;j++){
-                rp[i]^=m[i][j];
-                cp[j]^=m[i][j];
+        // Sender matrix
+        for (int i = 0; i < r; i++)
+            for (int j = 0; j < c; j++)
+                send[i][j] = s.nextInt();
+
+        // Sender parity
+        int[] rp = new int[r], cp = new int[c];
+        for (int i = 0; i < r; i++)
+            for (int j = 0; j < c; j++){
+                rp[i] ^= send[i][j];
+                cp[j] ^= send[i][j];
             }
 
-        System.out.println("Row Parity: " + Arrays.toString(rp));
-        System.out.println("Col Parity: " + Arrays.toString(cp));
+        // Receiver matrix (possibly corrupted)
+        for (int i = 0; i < r; i++)
+            for (int j = 0; j < c; j++)
+                recv[i][j] = s.nextInt();
 
-        // receiver: recheck
-        int[] rr=new int[r], cr=new int[c];
-        for(int i=0;i<r;i++)
-            for(int j=0;j<c;j++){
-                rr[i]^=m[i][j];
-                cr[j]^=m[i][j];
+        // Receiver parity
+        int[] rr = new int[r], cr = new int[c];
+        for (int i = 0; i < r; i++)
+            for (int j = 0; j < c; j++){
+                rr[i] ^= recv[i][j];
+                cr[j] ^= recv[i][j];
             }
 
-        System.out.println(Arrays.equals(rp,rr) && Arrays.equals(cp,cr)
-                ? "No Error" : "Error Detected");
+        System.out.println(
+            Arrays.equals(rp, rr) && Arrays.equals(cp, cr)
+            ? "No Error" : "Error Detected"
+        );
     }
 }
