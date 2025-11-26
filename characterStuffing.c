bd97ccf6f641cfd0d67a20a0dae52f7823838b77
yuva-123456 <yashu.spam2006@gmail.com>
2025-11-26 07:53:37 +0530

    Update characterStuffing.c

--- characterStuffing.c
+++ characterStuffing.c
@@ -5,32 +5,24 @@ int main() {
     char data[100], stuffed_data[200];
     int index = 0;
 
-    printf("Enter the data (with FLAG or ESC): ");
+    printf("Enter the input data (may contain FLAG or ESC): ");
     scanf("%s", data);
 
     for (int i = 0; i < strlen(data); i++) {
-        if (i < strlen(data) - 3 && strncmp(&data[i], "FLAG", 4) == 0) {
-            stuffed_data[index++] = 'E';
-            stuffed_data[index++] = 'S';
-            stuffed_data[index++] = 'C';
-            stuffed_data[index++] = 'F';
-            stuffed_data[index++] = 'L';
-            stuffed_data[index++] = 'A';
-            stuffed_data[index++] = 'G';
-            i += 3;  
-        }
-
-        else if (i < strlen(data) - 2 && strncmp(&data[i], "ESC", 3) == 0) {
-            stuffed_data[index++] = 'E';
-            stuffed_data[index++] = 'S';
-            stuffed_data[index++] = 'C';
-            i += 2;  
+        if (i <= strlen(data) - 4 && strncmp(&data[i], "FLAG", 4) == 0) {
+            strcpy(&stuffed_data[index], "ESCFLAG");
+            index += 7;
+            i += 3;
+        } else if (i <= strlen(data) - 3 && strncmp(&data[i], "ESC", 3) == 0) {
+            strcpy(&stuffed_data[index], "ESCESC");
+            index += 6;
+            i += 2;
         } else {
             stuffed_data[index++] = data[i];
         }
     }
 
-    stuffed_data[index] = '\0';  
+    stuffed_data[index] = '\0';
 
     printf("Original Data: %s\n", data);
     printf("Stuffed Data: %s\n", stuffed_data);
